73a3f4d938efb813bb9c12a0bd24bd4dc7031b2f
burcineren <burcin.ern969@gmail.com>
2025-03-07 14:42:21 +0300

    feat: login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [HomeComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -8,34 +8,23 @@ import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'login',
-  imports: [NzFormModule,
-    NzInputModule,
-    NzButtonModule,
-    NzIconModule,
-    ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm: FormGroup;
-  constructor(private fb: FormBuilder, private router: Router) {
+
+  constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
 
-  onSubmit() {
+  onSubmit(): void {
     if (this.loginForm.valid) {
-      const { username, password } = this.loginForm.value;
-
-      // 🔒 Basit Kullanıcı Kontrolü
-      if (username === 'admin' && password === 'admin') {
-        alert('Giriş Başarılı!');
-        this.router.navigate(['/dashboard']);
-      } else {
-        alert('Kullanıcı adı veya şifre yanlış!');
-      }
+      console.log('Form Submitted:', this.loginForm.value);
     }
   }
 }
