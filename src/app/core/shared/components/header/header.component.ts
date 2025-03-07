7e0e91739e163e1aedbc85a8756a78f815ba57c4
burcineren <burcin.ern969@gmail.com>
2025-03-07 14:22:56 +0300

    feat: header component

--- src/app/core/shared/components/header/header.component.ts
+++ src/app/core/shared/components/header/header.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { NzDrawerModule } from 'ng-zorro-antd/drawer';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'header',
-  imports: [NzLayoutModule,
-    NzMenuModule,
-    NzIconModule,
-    NzDrawerModule],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
