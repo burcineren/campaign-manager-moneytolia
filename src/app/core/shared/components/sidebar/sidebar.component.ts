d42b770d9f9f8b2cbb7f3d18bd9584d3bd073cad
burcineren <burcin.ern969@gmail.com>
2025-03-07 14:23:16 +0300

    feat: sidebar component

--- src/app/core/shared/components/sidebar/sidebar.component.ts
+++ src/app/core/shared/components/sidebar/sidebar.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { HeaderComponent } from '../header/header.component';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+
 
 @Component({
   selector: 'sidebar',
-  imports: [NzMenuModule, NzLayoutModule,
-    NzIconModule, NzBreadCrumbModule],
+  imports: [],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
