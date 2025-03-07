467704c7e97c3e924bb86705d2cc04706ca04ddc
burcineren <burcin.ern969@gmail.com>
2025-03-08 00:24:23 +0300

    fix: header and create campaign page

--- src/app/pages/create-campaign/create-campaign.component.ts
+++ src/app/pages/create-campaign/create-campaign.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Store } from '@ngxs/store';
 
 @Component({
   selector: 'app-create-campaign',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-campaign.component.html',
   styleUrl: './create-campaign.component.scss'
 })
 export class CreateCampaignComponent {
+  campaignForm: FormGroup;
+  constructor(private fb: FormBuilder, private store: Store, private router: Router) {
+    this.campaignForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
+  onSubmit() {
 
+  }
 }
