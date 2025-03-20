70e7df8697b85119562e5ce25f912ec058f5ae8a
burcineren <burcin.ern969@gmail.com>
2025-03-20 22:36:02 +0300

    fix: update css

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
-import { AuthStateModel } from '../states/auth-state/auth.type';
 
 @Injectable({
   providedIn: 'root'
--- src/app/pages/home/home.routes.ts
+++ src/app/pages/home/home.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { CampaignsComponent } from '../campaigns/campaigns.component';
+import { AuthGuard } from '../../core/guards/auth.guard';
 
 export const HomePageRoutes: Routes = [
   {
     path: '',
+    canActivate: [AuthGuard],
     component: HomeComponent,
   },
 ];
